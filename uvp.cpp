b6f2c52aea8f861b4bcaeea0af5da6b837a2600d
Oleg Goncharov <ge75gil@mytum.de>
2020-05-04 17:00:24 +0200

    Modified max_abs_velocity function: 1) removed unnecessary parameters from the function; 2) modified abs_value finding algorithm to make it more readable using pointer dereferencing. Currently the funtion is overloaded, but the previous one can be removed completely

--- uvp.cpp
+++ uvp.cpp
@@ -47,6 +47,25 @@ static bool abs_compare(int a, int b)
 }
 
 
+// Changed the function signature: removed imax and jmax - they are already contained in Grid
+// and can be acessed with imax() and jmax() methods. And jmax value is not used here at all.
+// Used *pointer to access the abs_max_value directly
+// Avoided creating additional vectors in the loop to improve performance and make code more readable
+// [Oleg]
+
+double max_abs_velocity(Grid& grid, velocity_type type) {
+    matrix<double> current_velocity; //matrix of current velocity U or V on grid
+    grid.velocity(current_velocity, type); //assigns velocity U or V to current_velocity
+    
+    // Vector of maximum velocity values in every row (including boundaries, i.e. imaxb):
+    std::vector<double> max_abs_value_per_row(grid.imaxb(), 0);
+
+    for (int i = 0; i < grid.imaxb(); ++i)
+        max_abs_value_per_row.at(i) = *std::max_element(current_velocity.at(i).begin(), current_velocity.at(i).end(), abs_compare);
+
+    return *std::max_element(max_abs_value_per_row.begin(), max_abs_value_per_row.end());
+}
+
 
 double max_abs_velocity(int imax, int jmax, Grid &grid, velocity_type type){
     matrix<double> current_velocity; //matrix of current velocity U or V on grid
@@ -75,12 +94,18 @@ void calculate_dt(double Re, double tau, double *dt, double dx, double dy, int i
     double max_abs_V = max_abs_velocity(imax, jmax, grid, velocity_type::V);
 
     //first stability conditon
-    double condition1 = 0.5 * Re * (dx * dx) * (dy * dy) / ((dx * dx) + (dy * dy));
+
+    double condition1 = 0.5 * Re * (dx * dx) * (dy * dy) / ((dx * dx) + (dy * dy));  
 
 
     //check if CFL stability conditions are too small, then just use first stability condition
-    //I am not sure if that's the best way to so it
-    if (max_abs_V < 0.01 or max_abs_U < 0.01) {
+    //I am not sure if that's the best way to do it
+    
+    // Decreased it a bit and replace logical OR operator from "or" to "||" to be consisntent
+    // with the project code, and avoid the issue I have with my configuration (it shows an error with "or")
+    // [Oleg]
+    
+    if (max_abs_V < 1e-06 || max_abs_U < 1e-06) {
         *dt = condition1;
     }
 
