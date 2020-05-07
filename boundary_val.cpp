357f64777ea89b3e54add5e62e84a78bb90a43b8
Oleg Goncharov <ge75gil@mytum.de>
2020-05-07 19:10:50 +0200

    Removed comments before merging into the master branch

--- boundary_val.cpp
+++ boundary_val.cpp
@@ -18,24 +18,24 @@ void boundaryvalues(int imax, int jmax, Grid& grid) {
     // ---BOTTOM--- //
     for (int i = 0; i < grid.imaxb(); i++) {
         u_velocity.at(i).at(0) = -u_velocity.at(i).at(1);
-        v_velocity.at(i).at(0) = 0; // shouldn't it be v_velocity.at(i+1).at(0) and thus only velocity ON physical boundary is zero? [Adrian]
+        v_velocity.at(i).at(0) = 0;
     }
 
     // ---LEFT--- //
     for (int j = 0; j < grid.jmaxb(); j++) {
-        u_velocity.at(0).at(j) = 0; // shouldn't it be u_velocity.at(0).at(j+1) and thus only velocity ON physical boundary is zero? [Adrian]
+        u_velocity.at(0).at(j) = 0;
         v_velocity.at(0).at(j) = -v_velocity.at(1).at(j);
     }
 
     // ---RIGHT--- //
     for (int j = 0; j < grid.jmaxb(); j++) {
-        u_velocity.at(imax + 1).at(j) = 0; //is this boundary condition needed at all? [Adrian]
+        u_velocity.at(imax + 1).at(j) = 0;
         v_velocity.at(imax + 1).at(j) = -v_velocity.at(imax).at(j);
     }
 
     // Note: this is related to internal most right cells of the physical domain
-    for (int j = 1; j <= jmax; j++) { //we should use SAME parameters in every loop: (either grid.jmaxb() or jmax) for consistency
-        u_velocity.at(imax).at(j) = 0; //I think that's correct
+    for (int j = 1; j <= jmax; j++) {
+        u_velocity.at(imax).at(j) = 0;
     }
 
     // ---TOP--- //
@@ -43,12 +43,12 @@ void boundaryvalues(int imax, int jmax, Grid& grid) {
     // u_wall may be later provided in the input txt read file
 
     for (int i = 0; i < grid.imaxb(); i++) {
-        v_velocity.at(i).at(jmax + 1) = 0; //is this condition needed at all?  [Adrian]
-        u_velocity.at(i).at(jmax + 1) = 2 - u_velocity.at(i).at(jmax); //shouldn't it be u_velocity.at(i+1).at(jmax + 1) = 2 - u_velocity.at(i+1).at(jmax) here? [Adrian]
+        v_velocity.at(i).at(jmax + 1) = 0;
+        u_velocity.at(i).at(jmax + 1) = 2 - u_velocity.at(i).at(jmax);
     }
     // Note: this is related to internal most upper cells of the physical domain
     for (int i = 1; i <= imax; i++)
-        v_velocity.at(i).at(jmax) = 0; //I think that's correct
+        v_velocity.at(i).at(jmax) = 0;
 
     grid.set_velocity(u_velocity, velocity_type::U);
     grid.set_velocity(v_velocity, velocity_type::V);
@@ -61,7 +61,6 @@ void boundaryvalues(int imax, int jmax, Grid& grid) {
     matrix<double> pres;
     grid.pressure(pres);
 
-    //looks good to me :)  [Adrian]
     for (int j = 1; j <= jmax; j++)
     {
         pres.at(0).at(j) = pres.at(1).at(j);                // LEFT
