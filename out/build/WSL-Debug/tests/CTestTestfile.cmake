0226066b80936516df0c111680e0d570f9da3561
ge38tob <adrian.salah@tum.de>
2020-05-12 11:04:18 +0200

    removed out directory from repo

--- out/build/WSL-Debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Issa Saba/Documents/Source/Repos/GroupCCFDLab/tests
-# Build directory: /mnt/c/Users/Issa Saba/Documents/Source/Repos/GroupCCFDLab/out/build/WSL-Debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example_test "/mnt/c/Users/Issa Saba/Documents/Source/Repos/GroupCCFDLab/out/build/WSL-Debug/tests/test_factorial")
-set_tests_properties(example_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Issa Saba/Documents/Source/Repos/GroupCCFDLab/tests/CMakeLists.txt;11;add_test;/mnt/c/Users/Issa Saba/Documents/Source/Repos/GroupCCFDLab/tests/CMakeLists.txt;0;")
