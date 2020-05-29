61e94d0dbe385d29e9e00a69ee4a9a42f49f1bbb
ge38tob <adrian.salah@tum.de>
2020-05-29 10:35:00 +0200

    removed unnessesary folders from repo

--- cmake-build-release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrian/CFDLab/cfdlabcodeskeleton
-# Build directory: /home/adrian/CFDLab/cfdlabcodeskeleton/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- cmake-build-release/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adrian/CFDLab/cfdlabcodeskeleton/tests
-# Build directory: /home/adrian/CFDLab/cfdlabcodeskeleton/cmake-build-release/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(example_test "/home/adrian/CFDLab/cfdlabcodeskeleton/cmake-build-release/tests/test_factorial")
-set_tests_properties(example_test PROPERTIES  _BACKTRACE_TRIPLES "/home/adrian/CFDLab/cfdlabcodeskeleton/tests/CMakeLists.txt;11;add_test;/home/adrian/CFDLab/cfdlabcodeskeleton/tests/CMakeLists.txt;0;")
