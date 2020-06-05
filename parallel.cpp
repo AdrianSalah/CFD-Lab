99a7bbddee0394fc942b1532df82a16fcb8a8de5
ge38tob <adrian.salah@tum.de>
2020-06-05 14:40:37 +0200

    indices for subdomains are calculated and stored in arrays. Indices for every subdomain theen send to the process assigned to this subdomain. Subdomain is uniformly split in rectangles even if imaxb, jmaxb is not divisible by iproc, jproc. Neighbour ranks are assigned for the current process if not outside of the domain.

--- parallel.cpp
+++ parallel.cpp
@@ -1,4 +1,5 @@
 #include "parallel.hpp"
+#include <math.h>
 #include <mpi.h>
 
 void init_parallel(
@@ -17,49 +18,122 @@ void init_parallel(
         int *rank_t,
         int *omg_i,
         int *omg_j,
-        int num_proc){
-
-    //*il = 0, *jb = 0;
-    int i_delta = (jmax + 2) / iproc, j_delta = (imax + 2) / jproc;
-    *omg_i = 0, *omg_j = 0;
-
-    if (num_proc > 1) {
-
-        //il, ir, jb, jt, rank_r, rank_b, ...are all send buffers
-        if (*myrank == 0) {
-            for (int j = 0; j < jproc; j++) {
-                for (int i = 0; i < iproc; i++) {
-                    int proc = i + j * iproc + 1 ; //start sending to proc = 1
-
-                    *il = i * i_delta;
-                    *ir = i * i_delta + i_delta;
-
-                    *jb = j * j_delta;
-                    *jt = j * j_delta + j_delta;
-
-                    *omg_i = i;
-                    *omg_j = j;
+        int num_proc) {
 
 
-                    if (*il == 0){*rank_l = MPI_PROC_NULL;}
-                    if (*jb == 0){*rank_b = MPI_PROC_NULL;}
-                    if (*ir == imax + 1){*rank_r = MPI_PROC_NULL;}
-                    if (*jt == jmax + 1){*rank_t = MPI_PROC_NULL;}
-                    else{
-                        //...
-                    }
-                    if(proc < num_proc) {
-                        MPI_Send(il, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                        MPI_Send(ir, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                        MPI_Send(jb, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                        MPI_Send(jt, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                        MPI_Send(omg_i, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                        MPI_Send(omg_j, 1, MPI_INT, proc, proc, MPI_COMM_WORLD);
-                    }
-                }
+    // compute indices for subdomains
+    float i_delta_f = static_cast<float>(imax + 2) / static_cast<float>(iproc);
+    float j_delta_f = static_cast<float>(jmax + 2) / static_cast<float>(jproc);
+    int i_delta = ceil(i_delta_f);
+    int j_delta = ceil(j_delta_f);
+    int i_delta_last_col = (imax + 2) - (iproc - 1) * i_delta;
+    int j_delta_last_row = (jmax + 2) - (jproc - 1) * j_delta;
+
+    int ir_arr[iproc], il_arr[iproc], jb_arr[jproc], jt_arr[jproc];
+
+    /* omg_i = {1, 2,..., iproc}, omg_j = {1, 2,..., jproc} */
+    int omg_i_arr[iproc], omg_j_arr[jproc];
+
+    // store indices for columns
+    for (int i = 0; i < iproc - 1; i++) {
+        il_arr[i] = i * i_delta;
+        ir_arr[i] = i * i_delta + i_delta;
+
+        omg_i_arr[i] = i + 1;
+    }
+    // store indices for last column
+    il_arr[iproc - 1] = (iproc - 1) * i_delta;
+    ir_arr[iproc - 1] = (iproc - 1) * i_delta + i_delta_last_col;
+
+    omg_i_arr[iproc - 1] = iproc;
+
+    // store indices for rows
+    for (int j = 0; j < jproc - 1; j++) {
+        jb_arr[j] = j * j_delta;
+        jt_arr[j] = j * j_delta + j_delta;
+
+        omg_j_arr[j] = j + 1;
+    }
+
+    //store indices for last row
+    jb_arr[jproc - 1] = (jproc - 1) * j_delta;
+    jt_arr[jproc - 1] = (jproc - 1) * j_delta + j_delta_last_row;
+
+    omg_j_arr[jproc - 1] = jproc;
+
+    // send the indices from rank 0 (master process) to the assigned processes
+    if (*myrank == 0) {
+        for (int j = 0; j < jproc; j++) {
+            for (int i = 0; i < iproc; i++) {
+                int proc = (i + j * iproc) % num_proc; //start sending to process 0
+                int tag = (i + j * iproc);
+
+                *omg_i = omg_i_arr[i];
+                *omg_j = omg_j_arr[j];
+
+                *il = il_arr[i];
+                *ir = ir_arr[i];
+                *jb = jb_arr[j];
+                *jt = jt_arr[j];
+
+
+                // check weather neighbour process is outside of boundary or not and specify ranks of neighbour processes
+                if (*il == 0){*rank_l = MPI_PROC_NULL;}
+                else{ *rank_l = proc - 1;}
+                if (*jb == 0){*rank_b = MPI_PROC_NULL;}
+                else{*rank_b = proc - iproc;}
+                if (*ir == imax+2){*rank_r = MPI_PROC_NULL;}
+                else{*rank_r = proc + 1;}
+                if (*jt == jmax+2){*rank_t = MPI_PROC_NULL;}
+                else{*rank_t = proc + iproc;}
+
+
+
+                MPI_Send(omg_i, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(omg_j, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+
+                MPI_Send(il, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(ir, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(jb, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(jt, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+
+                MPI_Send(rank_l, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(rank_r, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(rank_b, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+                MPI_Send(rank_t, 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+
             }
         }
+    }
+    // receive indices from rank 0
+    for (int j = 0; j < jproc; j++) {
+        for (int i = 0; i < iproc; i++) {
+            int tag = (i + j * iproc);
+            int proc = (i + j * iproc) % num_proc;
+            if (*myrank == proc) {
+                MPI_Recv(omg_i, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(omg_j, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+                MPI_Recv(il, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(ir, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(jb, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(jt, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                MPI_Recv(rank_l, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(rank_r, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(rank_b, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(rank_t, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+        }
+    }
+}
+
+
+
+
+
+        // suggestion [Oleg]
+        /*
         int x_cells_tot, y_cells_tot;
         int x_layer_full_width;
         int y_layer_full_width;
@@ -75,21 +149,10 @@ void init_parallel(
 
         x_layer_width_final = x_layer_full_width + x_uniform_add;
         x_layer_width_final_last_col = x_layer_width_final + x_layer_res % (iproc - 1);
+        */
 
 
 
-        for (int p = 1; p < num_proc; p++){
-            if (*myrank == p) {
-                MPI_Recv(il, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Recv(ir, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Recv(jb, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Recv(jt, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Recv(omg_i, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                MPI_Recv(omg_j, 1, MPI_INT, 0, p, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
-        }
-    }
-}
 
 void pressure_comm(
         double **P,
@@ -106,6 +169,8 @@ void pressure_comm(
         MPI_Status *status,
         int chunk){
 
+    /* --- this is kind of a sketch [Adrian] ---- */
+    /*
     int num_ghostcells_j = (jt +1) - (jb -1);
     int num_ghostcells_i = (ir +1) - (il -1);
     //fill send buffer for right ghost cell
@@ -115,5 +180,5 @@ void pressure_comm(
     //send ghost cells to right, receive ghost cells from left
     MPI_Sendrecv(bufSend, num_ghostcells_j, MPI_DOUBLE, rank_r, 123, bufRecv, num_ghostcells_j,
             MPI_DOUBLE, chunk, 123, MPI_COMM_WORLD, status );
-
+*/
 }
