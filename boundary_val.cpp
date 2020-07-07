b81010f5d9a602484805add7a3a0e8adb9af1d7b
Issa Saba <issasaba@hotmail.com>
2020-07-07 18:31:40 +0300

    fixed seg fault

--- boundary_val.cpp
+++ boundary_val.cpp
@@ -733,7 +733,7 @@ void spec_boundary_val(
 
         if (time < t_end * 0.4)
         {
-            for (int i = 9*3; i < 13*3; i++)
+            for (int i = 9+2*(scenarioSpec ==10); i < 13+ 2 * (scenarioSpec == 10); i++)
             {
                 conc_A.at(i).at(1) = C_inject[ID::A];
                 conc_B.at(i).at(1) = C_inject[ID::B];
